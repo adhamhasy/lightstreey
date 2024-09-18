11c05f1c09fe17dafbfa689a2e4bd3cb444ac13b
adhamhasy <122464685+adhamhasy@users.noreply.github.com>
2024-09-19 00:05:46 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function red() {
+    console.log(red)
+
+    let x = new Audio("./sound.jpg")
+    x.play
+}
+function yellow() {
+    console.log(yellow)
+
+    let y= new Audio("./sound.jpg")
+    y.play
+} function green() {
+    console.log(green)
+
+    let z = new Audio("./sound.jpg")
+    x.play
+}
